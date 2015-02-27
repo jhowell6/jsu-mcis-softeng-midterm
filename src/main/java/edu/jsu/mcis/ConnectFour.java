7ae782d953f2b7f91398c5152e8cbb98a3eb5aac
jhowell6 <jhowell6@jsu.edu>
2015-02-27 12:15:20 -0600

    final
    done to what I could do

--- src/main/java/edu/jsu/mcis/ConnectFour.java
+++ src/main/java/edu/jsu/mcis/ConnectFour.java
@@ -2,20 +2,30 @@ package edu.jsu.mcis;
 
 public class ConnectFour {
     public enum Location {EMPTY, RED, BLACK};
-    
+    public static final int COLS = 7;
+	private final int ROWS = 6;
+	private Location [][] board;
+	private boolean redTurn;
+	
     public ConnectFour() {
-        
+        board = new Location[ROWS][COLS];
+		for(int i = 0; i < board.length; i++){
+			for(int j = 0; j < board[i].length; j++){
+				board[i][j] = Location.EMPTY;
+			}
+		}
+		redTurn = true;
     }
     
     public Location getTopOfColumn(int column) {
-
+		return Location.EMPTY;
     }
     
     public int getHeightOfColumn(int column) {
-        
+        return 0;
     }
     
-    public void dropToken(int column) {
-        
+    public boolean dropToken(int column) {
+       	return true;
     }
 }
--- src/test/java/edu/jsu/mcis/ConnectFourTest.java
+++ src/test/java/edu/jsu/mcis/ConnectFourTest.java
@@ -7,7 +7,7 @@ public class ConnectFourTest{
 	@Test
     public void testNewGameBoardShouldBeEmpty() {
         ConnectFour c = new ConnectFour();
-        for(int col = 0; col < 7; col++) {
+        for(int col = 0; col < ConnectFour.COLS; col++) {
             assertEquals(ConnectFour.Location.EMPTY, c.getTopOfColumn(col));
             assertEquals(0, c.getHeightOfColumn(col));
         }
@@ -16,7 +16,8 @@ public class ConnectFourTest{
     @Test
     public void testDropRedTokenInThirdColumnTheBlackTokenOnTopOfIt() {
         ConnectFour c = new ConnectFour();
-        c.dropToken(2);
+		boolean ableToDropToken = c.dropToken(2);
+        assertTrue(ableToDropToken);
         assertEquals(ConnectFour.Location.RED, c.getTopOfColumn(2));
         assertEquals(1, c.getHeightOfColumn(2));
         c.dropToken(2);
@@ -24,9 +25,19 @@ public class ConnectFourTest{
         assertEquals(2, c.getHeightOfColumn(2));
     }
     
-    @Test(expected=ColumnFullException.class)
+    @Test//(expected=ColumnFullException.class)
     public void testDroppingOnAFullColumnShouldThrowException() {
-        
+        ConnectFour c = new ConnectFour();
+		c.dropToken(col);
+		c.dropToken(col);
+		c.dropToken(col);
+		c.dropToken(col);
+		c.dropToken(col);
+		boolean ableToDropToken = c.dropToken(col);
+		assertTrue(ableToDropToken);
+		ableToDropToken = c.dropToken(col);
+		assertFalse(ableToDropToken);
+		
     }
     
 }
